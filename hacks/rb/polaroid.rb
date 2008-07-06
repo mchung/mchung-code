110fa07eb943c1a42b1926cdff298e2c1f9141bc
Marc Chung <mchung@stargate.local>
2008-07-06 11:48:34 -0700

    Idiomatic ruby style

--- hacks/rb/polaroid.rb
+++ hacks/rb/polaroid.rb
@@ -1,40 +1,54 @@
 require "rubygems"
-require "RMagick"
+require "rmagick"
+require 'pp'
 
-if !ARGV[0]
-    puts "Usage: polaroid.rb path-to-image"
-    exit
+def exit_with_help_message
+  puts "Usage: #{$0} /path/to/image.png"
+  exit
 end
 
-image = Magick::Image.read(ARGV[0]).first
+module Magick
+  class Image
+    def polaroidify
+      image = self
+      image.border!(18, 18, "#f0f0ff")
+      image.background_color = "none"
 
-image.border!(18, 18, "#f0f0ff")
+      # Warpy
+      amplitude = image.columns * 0.01
+      wavelength = image.rows  * 2
+      image.rotate!(90)
+      image = image.wave(amplitude, wavelength)
+      image.rotate!(-90)
+      
+      # Shadowy
+      shadow = image.flop
+      shadow = shadow.colorize(1, 1, 1, "gray75")
+      shadow.background_color = "none"
+      shadow.border!(10, 10, "white")
+      shadow = shadow.blur_image(0, 3)
+      
+      # Mergy
+      image = shadow.composite(image, -amplitude/2, 5, Magick::OverCompositeOp)
+      image.background_color = "white"
+      image.rotate!(-5)
+      image.trim!
+      
+      image
+    end    
+  end
+end
 
-# Bend the image
-image.background_color = "none"
+def write_to_file!(image)
+  out = @image_path.sub(/\./, "-print.")
+  puts "Writing #{out}"
+  image.write(out)
+end
 
-amplitude = image.columns * 0.01        # vary according to taste
-wavelength = image.rows  * 2
+if $0 == __FILE__
+  exit_with_help_message unless ARGV[0] && File.exists?(ARGV[0])
 
-image.rotate!(90)
-image = image.wave(amplitude, wavelength)
-image.rotate!(-90)
-
-# Make the shadow
-shadow = image.flop
-shadow = shadow.colorize(1, 1, 1, "gray75")     # shadow color can vary to taste
-shadow.background_color = "none"       # was "none"
-shadow.border!(10, 10, "white")
-shadow = shadow.blur_image(0, 3)        # shadow blurriness can vary according to taste
-
-# Composite image over shadow. The y-axis adjustment can vary according to taste.
-image = shadow.composite(image, -amplitude/2, 5, Magick::OverCompositeOp)
-
-image.rotate!(-5)                       # vary according to taste
-image.trim!
-
-# Add -print to image basename, write to file.
-out = ARGV[0].sub(/\./, "-print.")
-puts "Writing #{out}"
-image.write(out)
-
+  @image_path = ARGV[0]
+  image = Magick::Image.read(@image_path).first.polaroidify
+  write_to_file!(image)
+end
